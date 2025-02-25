94f7c561988ad19fa2f9975fd8a63e3a51f3aacf
Geronimo Viscontti <visconttig@gmail.com>
2025-02-25 17:36:08 -0300

    Add display list functionality.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let friends = [];
+const LISTA_AMIGOS = "listaAmigos";
 
 async function getInputValue(input_id){
     let input_value = await document.getElementById(input_id).value;
@@ -8,22 +9,42 @@ async function getInputValue(input_id){
 }
 
 
-/* Regex 
-value.match(/^(?!\s*$).+|^\s+$/i)
-*/
-
-
 
 async function checkInputValue(value){
-  //  alert(`value: ${value}`);
-    if(value.length === 0 || value.match(/^\s+$/i) ){
-        return alert(`Por favor, ingresa un nombre válido. \nNombre ingresado: ${value}.`);
+    let lowerCaseValue = value.toLowerCase(value);
+
+    if((lowerCaseValue.length < 3) || lowerCaseValue.match(/^\s+$/i)){
+        alert(`El nombre ingresado debe contener al menos 3 carácteres. \nNombre ingresado: ${value}.`);
+        return Promise.reject("Not enough characters.");
+    } else if(lowerCaseValue.length >= 24){
+        alert(`El nombre ingresado debe contener menos de 24 carácteres.`);
+        return Promise.reject("Too much characters.");     
+        /* TO-DO: Correct case-sensitive matching */
+    } else if(friends.includes(value)) {
+        alert(`El nombre ingresado ya existe: ${value}`);
+        return Promise.reject("Name already exists.");
     } else {
-  //      alert("GOOD");
         return value;
     }
 }
 
-function agregarAmigo(input_id){
-    getInputValue(input_id);
+async function agregarAmigo(input_id){
+    let friend = await getInputValue(input_id);
+
+    if(friend.length > 0){
+        friends.push(friend);
+    }
+    displayFriends(friend);
+}
+
+
+
+
+function displayFriends(friend){
+    const friendsList = document.getElementById(LISTA_AMIGOS);
+    //for (let friend of friends){
+        let node = document.createElement("li");
+        node.innerHTML = `${friend}`;
+        friendsList.appendChild(node);
+    //}
 }
